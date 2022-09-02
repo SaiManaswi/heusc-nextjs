3fbf6d841704062c22f63e286f6bdca8d953080c
SaiManaswi <saimanaswi@yahoo.com>
2022-09-02 18:58:02 +0530

    WebSocket Test in heroku

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,24 +1,20 @@
-import {Server} from 'socket.io'
+import { Server } from 'Socket.IO'
 
-var gpos = 0
-const SocketHandel = (req,res)=>{
-    if(res.socket.server.io){
-        console.log("Already Exits")
-    }
-    else{
-        console.log(`Creating new Server`)
-        const io = new Server(res.socket.server)
-        res.socket.server = io
-        io.on('connection',socket =>{
-            socket.on('player_move', (pos) => {
-                pos = (pos+1) % 40
-                console.log(pos)
-                socket.broadcast.emit('setpos',pos) //--> is it called everytime??
-            })
-        })
-    }
-    res.end()
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('input-change', msg => {
+        socket.broadcast.emit('update-input', msg)
+      })
+    })
+  }
+  res.end()
 }
 
-
-export default SocketHandel
+export default SocketHandler
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,37 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import io from 'Socket.IO-client'
+let socket;
 
-export default function Home(props) {
+const Home = () => {
+  const [input, setInput] = useState('')
+
+  useEffect(() => {socketInitializer()}, [])
+
+  const socketInitializer = async () => {
+    await fetch('/api/socket');
+    socket = io()
+
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+
+    socket.on('update-input', msg => {
+      setInput(msg)
+    })
+  }
+
+  const onChangeHandler = (e) => {
+    setInput(e.target.value)
+    socket.emit('input-change', e.target.value)
+  }
+
   return (
-    <>
-      <Head>
-        <title>HEUSC App</title>
-      </Head>
-      <main>
-        <h1>Hello</h1>
-      </main>
-    </>
+    <input
+      placeholder="Type something"
+      value={input}
+      onChange={onChangeHandler}
+    />
   )
 }
+
+export default Home;
